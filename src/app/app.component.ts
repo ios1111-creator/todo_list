b2c0bed1f3768e9d597d20acfec3b1583a64ba42
pawlika <pawlik.addam@gmail.com>
2022-04-12 16:15:55 +0200

    change css and add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, QueryList, ViewChild} from '@angular/core';
+import {Component,  OnInit,} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,28 +8,36 @@ import {Component, ElementRef, OnInit, QueryList, ViewChild} from '@angular/core
 export class AppComponent implements OnInit {
   title = 'todo-list';
   text: string = '';
-  data = new Date();
-  id:number=0;
-  @ViewChild('done', {static: false}) done!: QueryList<ElementRef>
+  date = new Date();
+  id:number=3;
+  config:{[key:string]:string}|null=null;
   todoLists = [
     {
       id: 0,
       name: 'Buy car ',
-      data: new Date(2022, 12, 22),
+      date: new Date(2022, 12, 22),
       done: false
     }, {
       id: 1,
       name: 'Buy car ',
-      data: new Date(2022, 12, 22),
+      date: new Date(2022, 12, 22),
       done: false
     }, {
       id: 2,
       name: 'Buy car ',
-      data: new Date(2022, 12, 22),
+      date: new Date(2022, 12, 22),
       done: false
     },
   ]
-
+constructor() {
+setTimeout(()=>{
+  this.config={
+    title:'Todo list',
+    footer:'© Todo list  built in Angular.',
+    date: new Date().toDateString(),
+  }
+},2000)
+  }
   ngOnInit() {
 
   }
@@ -38,7 +46,7 @@ export class AppComponent implements OnInit {
     this.todoLists.push({
       id: this.id++,
       name: this.text,
-      data: this.data,
+      date: this.date,
       done: false
     })
     console.log(this.todoLists)
@@ -54,9 +62,12 @@ export class AppComponent implements OnInit {
       }
       return todo;
     })
+    console.log(this.todoLists)
   }
   onDelete(id:number){
     this.todoLists.splice(id,1)
   }
-
+  onDeleteAll(){
+    this.todoLists=[];
+  }
 }
